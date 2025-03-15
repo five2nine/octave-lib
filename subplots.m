1b254985e99519c8e637dc8b48d7703e612f2c02
Won Sunggyu <fabre@pusan.ac.kr>
2025-03-16 00:17:03 +0900

    subplots.m  ID 입력 기본값 결정

--- subplots.m
+++ subplots.m
@@ -5,10 +5,10 @@ function [fig, axes] = subplots(nrows, ncols, param_fig, param_axes)
     %   Octave
     %
     % 입력:
-    %   nrows      - 서브플롯의 행 개수
-    %   ncols      - 서브플롯의 열 개수
-    %   param_fig  - figure 설정을 위한 파라미터 리스트 (셀 배열)
-    %   param_axes - axes 설정을 위한 파라미터 리스트 (셀 배열)
+    %   nrows      - 서브플롯의 행 개수 (기본값: 1)
+    %   ncols      - 서브플롯의 열 개수 (기본값: 1)
+    %   param_fig  - figure 설정을 위한 파라미터 리스트 (셀 배열, 기본값: {})
+    %   param_axes - axes 설정을 위한 파라미터 리스트 (셀 배열, 기본값: {})
     %
     % 출력:
     %   fig  - 생성된 figure 핸들
@@ -18,12 +18,19 @@ function [fig, axes] = subplots(nrows, ncols, param_fig, param_axes)
     %   - 지정된 행(nrows)과 열(ncols) 크기로 서브플롯을 배치함
     %   - subplot 인덱스는 C 언어 스타일(행 우선) 순서로 배치됨
     %   - fig의 ID는 무작위로 생성됨 (hex 값 범위: F000 ~ FFFF)
+    %   - nrows, ncols, param_fig, param_axes가 없을 경우 기본값을 사용
     %
     % 사용 예시:
+    %   [fig, ax] = subplots();           % 1x1 서브플롯 생성
+    %   [fig, ax] = subplots(2, 3);       % 2x3 서브플롯 생성
+    %   [fig, ax] = subplots(2, 3, {'Size', [800, 600]});
     %   [fig, ax] = subplots(2, 3, {'Size', [800, 600]}, {'XGrid', 'on'});
-    %   plot(ax(1,1), x, y);
-    %   title(ax(1,1), 'Plot 1');
-    
+
+    if nargin < 1, nrows = 1; end
+    if nargin < 2, ncols = 1; end
+    if nargin < 3, param_fig = {"Size", [720, 480]}; end
+    if nargin < 4, param_axes = {"FontSize", 16, "NextPlot", "add", "XGrid", "on", "YGrid", "on"}; end
+
     % 랜덤한 figure ID 생성
     figid = randi([hex2dec('F000'), hex2dec('FFFF')]);
     
